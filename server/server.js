2766f49c3e3ceb88894a86c434c141152804f191
Chris <chris_jeong@brown.edu>
2023-05-30 21:15:01 -0700

    fixed

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 var cors = require('cors')
 var app = express();
 
-app.use(cors())
+app.use(cors());
+app.use(bodyParser.json());
 app.post("/api", (req, res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree"]})
+    const receivedJson = req.body;
+    console.log('Received JSON: ', receivedJson)
+    res.send('JSON Received Successfully')
 })
 
 app.listen(3001, () => {console.log("Server started on port 3001")})
